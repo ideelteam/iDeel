f51f3c61d8f811a40db705d2f3c8a1876bf01603
Benson Li <li_benson12@hotmail.com>
2017-04-30 11:00:55 -0700

    Fixed icons

---
+++ createDB/createiDeelData.js
@@ -0,0 +1,9 @@
+// db = db.getSiblingDB('ideeldb');
+// db.createCollection('jobs');
+// jobCollection = db.getCollection("jobs");
+// jobCollection.remove({});
+// jobCollection.insert(
+//     {
+
+//     }
+// )
